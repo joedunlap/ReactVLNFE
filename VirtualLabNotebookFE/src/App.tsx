f93c09525a453543b7798bf078fddb71aa038101
joedunlap <joedunlap26@gmail.com>
2024-05-25 21:27:25 -0500

    fixed issue with collapsable navbar not working

--- VirtualLabNotebookFE/src/App.tsx
+++ VirtualLabNotebookFE/src/App.tsx
@@ -1,4 +1,6 @@
 import "./App.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar';
 import ProjectPage from './pages/projectpage';
