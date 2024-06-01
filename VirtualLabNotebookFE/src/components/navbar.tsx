5988512732a23981ef92fa7edb6a41d71e8cee12
joedunlap <joedunlap26@gmail.com>
2024-06-01 16:09:39 -0500

    added current time to the navbar

--- VirtualLabNotebookFE/src/components/navbar.tsx
+++ VirtualLabNotebookFE/src/components/navbar.tsx
@@ -1,17 +1,30 @@
 import { Link } from 'react-router-dom';
 import flask from '../assets/images/flask.png';
+import { useEffect, useState } from 'react';
 
 const Navbar = () => {
+    const [currentTime, setCurrentTime] = useState(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentTime(new Date());
+        }, 1000); // Update every second
+
+        return () => clearInterval(interval); // Clean up function
+    }, []);
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/"><img src={flask} alt="flask logo" className="flask" style={{ width: '50px', height: 'auto' }}/></Link>
+                <Link className="navbar-brand" to="/">
+                    <img src={flask} alt="flask logo" className="flask" style={{ width: '50px', height: 'auto' }} />
+                </Link>
                 <Link className="navbar-brand" to="/">Virtual Lab Notebook</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
+                    <ul className="navbar-nav me-auto">
                         <li className="nav-item">
                             <Link className="nav-link" to="/">Home</Link>
                         </li>
@@ -22,6 +35,9 @@ const Navbar = () => {
                             <Link className="nav-link" to="/projects/new">Create New Project</Link>
                         </li>
                     </ul>
+                    <span className="navbar-text ms-auto">
+                        Current Time: {currentTime.toLocaleTimeString()}
+                    </span>
                 </div>
             </div>
         </nav>
