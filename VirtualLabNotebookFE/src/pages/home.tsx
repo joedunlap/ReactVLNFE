cce0e5e3beab827138ae5653769c3e3e24c4ded0
joedunlap <joedunlap26@gmail.com>
2024-06-23 14:37:43 -0500

    added a paragraph description to home page

--- VirtualLabNotebookFE/src/pages/home.tsx
+++ VirtualLabNotebookFE/src/pages/home.tsx
@@ -6,8 +6,10 @@ const Home = () => {
   return (
     <div className="container-fluid">
         <img src ={flask} alt="flask with green bubbly substance" className='flask' />
-      <h1>Welcome to the Virtual Lab Notebook</h1>
-      <p>This is the home page of your application.</p>
+      <h1>Welcome to the Virtual Lab Notebook!</h1>
+      <p>A digital platform to manage scientific research projects and data.</p>
+      <p>This is the home page of the application.</p>
+
       <Link to="/projects" className="btn btn-primary m-4">View Your Projects</Link>
       <Link to="/projects/new" className="btn btn-success m-4">Create a New Project</Link>
       <Footer />
